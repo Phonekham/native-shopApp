3421d5b50f060ea05fa15845365808dfd82caade
Phonekham <phone.madridista@gmail.com>
2019-11-24 15:42:54 +0700

    setup navigator

--- screens/shop/ProductsOverview.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { FlatList, View, Text } from "react-native";
-
-const ProductsOverview = props => {
-  return <FlatList></FlatList>;
-};
-
-export default ProductsOverview;
