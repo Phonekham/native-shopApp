bc8fbbf4a16a1fdfd41b227b9b1080680ead8034
Phonekham <phone.madridista@gmail.com>
2019-11-24 16:46:25 +0700

    Adding Touchable Components

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,28 +1,42 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  TouchableNativeFeedback
+} from "react-native";
 import Colors from "../../constants/Colors";
 
 const ProductItem = props => {
   return (
     <View style={styles.product}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: props.image }}></Image>
-      </View>
-      <View style={styles.details}>
-        <Text style={styles.title}>{props.title}</Text>
-        <Text style={styles.price}>{props.price.toFixed(2)}</Text>
-      </View>
-      <View style={styles.actions}>
-        <Button
-          color={Colors.primary}
-          title="View Details"
-          onPress={props.onViewDetail}
-        ></Button>
-        <Button
-          color={Colors.accent}
-          title="Go To Cart"
-          onPress={props.onAddToCart}
-        ></Button>
+      <View style={styles.touchable}>
+        <TouchableNativeFeedback onPress={props.onViewDetail} useForeground>
+          <View>
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={{ uri: props.image }}></Image>
+            </View>
+            <View style={styles.details}>
+              <Text style={styles.title}>{props.title}</Text>
+              <Text style={styles.price}>{props.price.toFixed(2)}</Text>
+            </View>
+            <View style={styles.actions}>
+              <Button
+                color={Colors.primary}
+                title="View Details"
+                onPress={props.onViewDetail}
+              ></Button>
+              <Button
+                color={Colors.accent}
+                title="Go To Cart"
+                onPress={props.onAddToCart}
+              ></Button>
+            </View>
+          </View>
+        </TouchableNativeFeedback>
       </View>
     </View>
   );
@@ -39,6 +53,10 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     height: 300,
     margin: 20
+  },
+  touchable: {
+    borderRadius: 10,
+    overflow: "hidden"
   },
   imageContainer: {
     width: "100%",
