f0fafa064d523b11c7bc0db028f8350b31fb68ca
Phonekham <phone.madridista@gmail.com>
2019-11-24 16:59:52 +0700

    ProductDetailScreen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, Button } from "react-native";
 import { useSelector } from "react-redux";
+import { ScrollView } from "react-native-gesture-handler";
 
 const ProductDetailScreen = props => {
   const productId = props.navigation.getParam("productId");
@@ -9,9 +10,17 @@ const ProductDetailScreen = props => {
   );
 
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image
+        style={styles.image}
+        source={{ uri: selectedProduct.imageUrl }}
+      ></Image>
+      <View style={styles.actions}>
+        <Button title="Add To Cart" onPress={() => {}}></Button>
+      </View>
+      <Text style={styles.price}>{selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
@@ -21,6 +30,26 @@ ProductDetailScreen.navigationOptions = navData => {
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: "center"
+  },
+  price: {
+    fontSize: 20,
+    color: "#888",
+    textAlign: "center",
+    marginVertical: 20
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginHorizontal: 20
+  }
+});
 
 export default ProductDetailScreen;
