926623e224fbc530117717876d5e81f6449dba83
Phonekham <phone.madridista@gmail.com>
2019-12-09 16:10:55 +0700

    Mapping Orders to Users

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -28,6 +28,14 @@ const OrdersScreen = props => {
     );
   }
 
+  if (orders.length === 0) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>No products found</Text>
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={orders}
--- store/actions/products.js
+++ store/actions/products.js
@@ -6,7 +6,8 @@ export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const SET_PRODUCTS = "SET_PRODUCTS";
 
 export const fetchProducts = () => {
-  return async dispatch => {
+  return async (dispatch, getState) => {
+    const userId = getState().auth.userId;
     try {
       const response = await fetch(
         "https://native-shopapp.firebaseio.com/products.json"
@@ -22,7 +23,7 @@ export const fetchProducts = () => {
         loadedProducts.push(
           new Product(
             key,
-            "u1",
+            resData[key].ownerId,
             resData[key].title,
             resData[key].imageUrl,
             resData[key].description,
@@ -31,7 +32,11 @@ export const fetchProducts = () => {
         );
       }
 
-      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+      dispatch({
+        type: SET_PRODUCTS,
+        products: loadedProducts,
+        userProducts: loadedProducts.filter(prod => prod.ownerId === userId)
+      });
     } catch (err) {
       throw err;
     }
@@ -58,6 +63,7 @@ export const createProduct = (title, description, imageUrl, price) => {
   return async (dispatch, getState) => {
     // Any async code you want
     const token = getState().auth.token;
+    const userId = getState().auth.userId;
     const response = await fetch(
       `https://native-shopapp.firebaseio.com/products.json?auth=${token}`,
       {
@@ -69,7 +75,8 @@ export const createProduct = (title, description, imageUrl, price) => {
           title,
           description,
           imageUrl,
-          price
+          price,
+          ownerId: userId
         })
       }
     );
@@ -84,7 +91,8 @@ export const createProduct = (title, description, imageUrl, price) => {
         title,
         description,
         imageUrl,
-        price
+        price,
+        ownerId: userId
       }
     });
   };
