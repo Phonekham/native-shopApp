8bd06f930128110b614ad0f33e25202bfc4ae5a5
Phonekham <phone.madridista@gmail.com>
2019-12-09 16:49:40 +0700

    Implementing Auto Login

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -12,6 +12,7 @@ import OrdersScreen from "../screens/shop/OrdersScreen";
 import UserProductsScreen from "../screens/user/UserProductsScreen";
 import EditProductScreen from "../screens/user/EditProductScreen";
 import AuthScreen from "../screens/user/AuthScreen";
+import StartupScreen from "../screens/user/StartupScreen";
 
 const defaultNavOptions = {
   headerStyle: {
@@ -98,6 +99,7 @@ const AuthNavigator = createStackNavigator({
 
 const MainNavigator = createSwitchNavigator(
   {
+    Startup: StartupScreen,
     Auth: AuthNavigator,
     Shop: ShopNavigator
   },
